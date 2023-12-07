f5dbc2865ad4a49b681c852dcf9583b1792d97ff
MatiasOrtega <mat-1998-ort@hotmail.com>
2023-12-06 21:34:19 -0300

    modifying the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,68 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react"
 
+import ButtonOptionList from "./components/ButtonOptionList"
+import ImageHanged from "./components/ImageHanged"
+import GetWordRandom from "./data/GetWordRandom"
+import Answer from "./components/Answer"
+
+import "./App.css"
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [attempts, setAttempts] = useState(8)
+  const [word, setWord] = useState(GetWordRandom())
+  const [hiddenWord, setHiddenWord] = useState(word.split("").map(() => "_ "))
+
+  const [buttonActive, setButtonActive] = useState('activo')
+  const [win, setWin] = useState(false)
+  const [lose, setLose] = useState(false)
+  console.log(word)
+  
+  useEffect(() => {
+    if (attempts <= 0) {
+      setLose(true)
+      setAttempts(0)
+    }
+  },[attempts])
+
+  useEffect(() => {
+    if (hiddenWord.join("") === word) {
+      setWin(true)
+    }
+  }, [hiddenWord])
+
+  const cheackLetter = (letter) => {
+    let letterCheack = letter.toLowerCase();
+    if (word.includes(letterCheack)) {
+      const newHiddenWord = [...hiddenWord]
+      word.split("").forEach((l, index) => {
+        if (l === letterCheack) {
+          newHiddenWord[index] = letterCheack
+        }
+      })
+      setHiddenWord(newHiddenWord)
+    }else{
+      setAttempts(attempts - 1)
+    }
+    setButtonActive(letter)
+  }
+
+  const resumeGame = () => {
+    setAttempts(8)
+    setWord(GetWordRandom())
+    setHiddenWord(word.split("").map(() => "_ "))
+    setWin(false)
+    setLose(false)
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>AHORCADO</h1>
+      <ImageHanged errorNumber={attempts}/>
+      <Answer win={win} lose={lose} word={word}/>
+      <h3>{hiddenWord}</h3>
+      <h3>Attemps: {attempts}</h3>
+      <ButtonOptionList cheackLetter={cheackLetter} buttonActive={buttonActive}/>
+      <button onClick={resumeGame}>Resume Game</button>
     </>
   )
 }
